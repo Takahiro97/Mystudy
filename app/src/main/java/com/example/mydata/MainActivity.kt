53c12ca35f4a4071bb431944f92da1864a89f098
Takahiro Kawahara <takahiro24dec@outlook.jp>
2020-08-04 06:32:59 +0900

    あ

--- app/src/main/java/com/example/mydata/MainActivity.kt
+++ app/src/main/java/com/example/mydata/MainActivity.kt
@@ -2,30 +2,27 @@ package com.example.mydata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import android.content.Intent
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import java.io.File
+import android.content.Context
 import java.io.BufferedReader
+import kotlinx.android.synthetic.main.activity_main.*
 
 const val EXTRA_MESSAGE="com.example.mydata.MASSAGE"
 
 class MainActivity : AppCompatActivity() {
     private val fileName="datafile.txt"
-    val dataEdit=findViewById(R.id.dataEdit) as EditText
-    val dataText=findViewById(R.id.dataText) as TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //
         val state = DataState(1, "name")
         dataText.text=state.name
     }
     //changeボタンの処理　　"(view:View)"を記述するとレイアウトページのonClickにおいて関数の指定が可能
+
     fun sendMessage(view: View){
         val message=dataEdit.text.toString()
         val intent = Intent(this, SubActivity::class.java).apply{
@@ -52,12 +49,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
     //ファイル保存
-    fun saveFile(file:String, str:String){
-        File(applicationContext.filesDir, file).writer().use {
-            it.write(str)
+    private fun saveFile(file:String, str:String){
+        applicationContext.openFileOutput(file, Context.MODE_PRIVATE).use {
+            it.write(str.toByteArray())
         }
     }
-    fun readFiles(file:String):String?{
+    private fun readFiles(file: String):String?{
         val readFile=File(applicationContext.filesDir, file)
         if(!readFile.exists()){
             Log.d("debug","No file exists")
